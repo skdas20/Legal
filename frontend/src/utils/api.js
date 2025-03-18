fbf5f37a102defa92a8b2d18872dc4da5fb3739d
Sumit Kumar Das <skdas5405@gmail.com>
2025-03-18 22:15:59 +0530

    All Set

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
+// Set the backend API URL to the Render deployment
+const API_URL = 'https://digilexaibackend.onrender.com/api';
 
 console.log('API URL:', API_URL); // Debug log
 
